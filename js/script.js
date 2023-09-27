a4efaa7d9b5c58eecdbf327f42ccdb62c2593c9e
pratikand7 <pratik.aneelam.97@gmail.com>
2023-09-27 18:49:50 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -2,9 +2,14 @@ const url = "https://raddythebrand.github.io/apex-legends/data.json"
 
 async function getLegends(){
     try {
-        
+
+        const res = await fetch(url);
+        const data = await res.json();
+
     } catch (error) {
         console.log(error)
         
     }
 }
+
+getLegends();
