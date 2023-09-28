90b122f2c0278a7b73fa9c82438055022eca6557
pratikand7 <pratik.aneelam.97@gmail.com>
2023-09-28 17:06:06 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 const url = "https://raddythebrand.github.io/apex-legends/data.json"
-const loader = document.getElementById('loading')
+const loader = document.getElementById('loading');
+const loaderFrame = document.getElementById('loading-frame')
 
 async function getLegends(){
     try {
 
-        const res = await fetch(url);
+        const respose = await fetch(url);
         const data = await res.json();
+        console.log(data);
+
+        if (respose){
+            loader.style.display = 'none';
+        }
 
     } catch (error) {
-        console.log(error)
-        
+        console.log(error);
+        loaderFrame.innerHTML = "Error"
     }
 }
 
